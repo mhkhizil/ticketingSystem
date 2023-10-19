767935ac15d908133a52df01abf83be998b60a78
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2023-10-19 15:16:39 +0630

    nav update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Path from './Routes/Path'
 
 const App = () => {
   return (
     <div>
-      
+      <Path/>
     </div>
   )
 }
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../Components/Nav'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Nav/>
+  This is home
+    </div>
+  )
+}
+
+export default HomePage
