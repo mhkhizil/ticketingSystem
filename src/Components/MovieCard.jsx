adbaaf8c45a5e54b731283735a46451a431dd7a1
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2023-10-25 21:15:27 +0630

    update

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const MovieCard = ({md}) => {
   return (
   < >
-  <img className=' w-[300px] h-[400px] rounded-xl' src={md?.moviePath} alt="" />
+<Link to={'/cinema'}>
+<img className=' w-[300px] h-[400px] rounded-xl' src={md?.moviePath} alt="" />
+</Link>
 
   </>
   )
