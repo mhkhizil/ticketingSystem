208e18a5c8ec919264fec04c74bd5bb5a7331809
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2023-10-29 14:30:50 +0630

    uodate

--- src/Components/CinemaList.jsx
+++ src/Components/CinemaList.jsx
@@ -6,12 +6,15 @@ const CinemaList = ({cl}) => {
  
   return (
     <>
-      <div className=' flex items-center  justify-evenly'>
-       <div>
+      <div className='  flex items-center  justify-evenly'>
+       <div className='w-[15%]'>
        <p>{cl?.CinemaName}</p>
        </div>
- <div>
+ <div className='w-[5%]'>
  <p><BiCurrentLocation/></p>
+ </div>
+ <div className=''>
+ <p>avdddd</p>
  </div>
       </div>
     </>
