75cd4a1ba1b757f0d59d8856b3bfb88a6d5fe752
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2023-12-27 15:16:51 +0630

    minor bug fixes

--- src/Components/CinemaList.jsx
+++ src/Components/CinemaList.jsx
@@ -8,25 +8,25 @@ import { addCinema, addRoom } from "../Services/dataSlice";
 
 const CinemaList = ({ cl, roomNames, mid }) => {
   const dispatch = useDispatch();
-  const data =useParams();
-
+  const data = useParams();
 
   //movie id por mu t p room id ko tite sit
   const sd = dataFetching("Tbl_MovieShowDate");
   //  console.log(roomNames);
- const testingFiletr=sd?.filter(s=>(
-  s?.MovieId ==data?.mid && s?.CinemaId ==cl?.CinemaId
- ));
- console.log(testingFiletr);
- const requiredRoomId=testingFiletr?.map(id=>id?.RoomId);
+  const testingFiletr = sd?.filter(
+    (s) => s?.MovieId == data?.mid && s?.CinemaId == cl?.CinemaId
+  );
+  console.log(testingFiletr);
+  const requiredRoomId = testingFiletr?.map((id) => id?.RoomId);
   console.log(requiredRoomId);
- const finalRoomName=roomNames?.filter((rd)=>requiredRoomId?.includes(rd?.RoomId));
- console.log(finalRoomName);
-//  console.log(testingFiletr);
+  const finalRoomName = roomNames?.filter((rd) =>
+    requiredRoomId?.includes(rd?.RoomId)
+  );
+  console.log(finalRoomName);
+  //  console.log(testingFiletr);
 
-
-//  console.log(testingFiletr);
-//  console.log(testingFiletr);
+  //  console.log(testingFiletr);
+  //  console.log(testingFiletr);
   const nav = useNavigate();
   // console.log(cl);
   // console.log(cinemaRoom);
@@ -36,53 +36,55 @@ const CinemaList = ({ cl, roomNames, mid }) => {
     window.location.href = `https://www.google.com/maps?q=${cinemaLocation}`;
   };
   return (
-    <>
-      <div className="  justify-center items-center  flex-1  xxss:flex xxss:items-center  xxss:justify-evenly">
-        <div className=" inline-flex  xxss:w-[20%] ">
-          <p className=" inline-flex    xxxs:text-sm sm:text-xl  font-bold">
-            {cl?.CinemaName}
-          </p>
-        </div>
-        <div className="inline-flex  mx-3 ">
-          <p
-            onClick={handleLocationClick}
-            className="  inline-flex xxxs:text-sm sm:text-2xl hover:text-red-500 hover:cursor-pointer "
-          >
-            <BiCurrentLocation />
-          </p>
-        </div>
-
-        <div className=" cursor-pointer ">
-          {finalRoomName?.map((r, i) => {
-         
-            return (
-              <p
-                onClick={() => {
-                  dispatch(
-                    addRoom({
-                      room: r?.RoomName
-                    })
-                  );
-                  nav(
-                    `/cinema/${mid?.mid}/scheduling/${cl?.CinemaId}/${r?.RoomId}`
-                  );
-                  dispatch(
-                    addCinema({
-                      cinema: cl?.CinemaName,
-                    })
-                  );
-                  document.getElementById("my_modal_1").showModal();
-                }}
-                key={i + 1}
-                className="bg-red-500 rounded-xl block  xxss:inline-flex text-white p-2 m-2 xxxs:text-sm sm:text-xl hover:opacity-60"
-              >
-                {r?.RoomName}
-              </p>
-            );
-          })}
-        </div>
+    <div className="   justify-evenly items-center  flex-1  ">
+      <div className=" group  relative inline-flex   ">
+        <img
+          src={cl?.cinemaImg}
+          className=" group-hover:opacity-40 w-[300px] h-[200px]"
+          alt=""
+        />
+        <p
+          onClick={handleLocationClick}
+          className=" hidden group-hover:block  hover:text-white absolute  text-3xl  top-28  left-32   text-red-500 hover:cursor-pointer "
+        >
+          <BiCurrentLocation />
+        </p>
       </div>
-    </>
+      <div className="  flex items-center justify-center  ">
+        <p className=" text-center inline-flex    xxxs:text-sm sm:text-xl  font-bold">
+          {cl?.CinemaName}
+        </p>
+      </div>
+
+      <div className="  w-[300px] flex items-center justify-center flex-wrap cursor-pointer ">
+        {finalRoomName?.map((r, i) => {
+          return (
+            <p
+              onClick={() => {
+                dispatch(
+                  addRoom({
+                    room: r?.RoomName,
+                  })
+                );
+                nav(
+                  `/cinema/${mid?.mid}/scheduling/${cl?.CinemaId}/${r?.RoomId}`
+                );
+                dispatch(
+                  addCinema({
+                    cinema: cl?.CinemaName,
+                  })
+                );
+                document.getElementById("my_modal_1").showModal();
+              }}
+              key={i + 1}
+              className="bg-red-500 rounded-xl block  xxss:inline-flex text-white p-2 m-2 xxxs:text-sm sm:text-xl hover:opacity-60"
+            >
+              {r?.RoomName}
+            </p>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
